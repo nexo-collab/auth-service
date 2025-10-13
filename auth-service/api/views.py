ff99d921b4cd6497e1300c107ceacd6b490e32c9
Paulo-Douglas <paulodouglasmdias@gmail.com>
2025-10-13 09:26:18 -0300

    feat(API): cria view de login com geração de token JWT #3

--- auth-service/api/views.py
+++ auth-service/api/views.py
@@ -1,7 +1,29 @@
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework import status
 from core.models import User
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class=RegisterSerializer
+    serializer_class = RegisterSerializer
+
+class LoginView(APIView):
+    
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }, status=status.HTTP_200_OK)
+
+        return Response({
+            "detail": "Credenciais inválidas."
+        }, status=status.HTTP_401_UNAUTHORIZED)
+        
