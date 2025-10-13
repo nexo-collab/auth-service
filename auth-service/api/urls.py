04db16878b20eaea8a432dbe682e38e1a56d5bb7
Paulo-Douglas <paulodouglasmdias@gmail.com>
2025-10-13 10:44:16 -0300

    feat(API): adiciona as rotas de listagem de usuários #19

--- auth-service/api/urls.py
+++ auth-service/api/urls.py
@@ -1,7 +1,9 @@
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, UserListAPIView, UserRetriveAPIView
 from django.urls import path
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='user-register'),
     path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/user/', UserListAPIView.as_view(), name='users'),
+    path('auth/user/<uuid:pk>/', UserRetriveAPIView.as_view(), name='user'),
 ]
