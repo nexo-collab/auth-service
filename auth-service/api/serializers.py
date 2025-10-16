536c5fd911fbde9a27b52889305fe41c507ae82e
Paulo-Douglas <paulodouglasmdias@gmail.com>
2025-10-16 09:42:47 -0300

    fix: muda id para user_id no serializers

--- auth-service/api/serializers.py
+++ auth-service/api/serializers.py
@@ -6,7 +6,7 @@ from core.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'role')  # Explicitly list safe fields
+        fields = ('user_id', 'email', 'role') 
 
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
